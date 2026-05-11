81c9664a9761b7aec3e52025c917e6cb5b32aeb3
Alanjavier22 <alanstar007@gmail.com>
2026-05-10 22:16:55 -0500

    feat: implementación y refinamiento técnico en menu.bat

--- menu.bat
+++ menu.bat
@@ -1,2 +1,3 @@
 @echo off
-powershell -ExecutionPolicy Bypass -File "%~dp0menu.ps1"
+chcp 65001 >nul
+powershell -NoProfile -ExecutionPolicy Bypass -Command "[Console]::OutputEncoding = [Console]::InputEncoding = [System.Text.Encoding]::UTF8; $global:SCRIPT_DIR = '%~dp0'.TrimEnd('\'); & ([scriptblock]::Create([System.IO.File]::ReadAllText('%~dp0menu.ps1', [System.Text.Encoding]::UTF8)))"
