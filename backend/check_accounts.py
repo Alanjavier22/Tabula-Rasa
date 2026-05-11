4539052c7e2b255a74ff3c4978a17f13f6ff869a
Alanjavier22 <alanstar007@gmail.com>
2026-05-10 22:16:26 -0500

    feat: implementación y refinamiento técnico en backend/check_accounts.py

--- backend/check_accounts.py
+++
@@ -1,5 +0,0 @@
-import requests
-
-accs = requests.get('http://127.0.0.1:8000/accounts/').json()
-for a in accs:
-    print(f'ID: {a["id"]}, Name: {a["name"]}, Balance: {a["balance"]}, Type: {a["account_type"]}')
